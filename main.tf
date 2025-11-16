9cb03bb1ed17dd4d8efce939b8ceb161454a1e06
testkdjojo <davidokiji131@gmail.com>
2025-11-16 23:10:54 +0000

    Update main.tf
    removal of all variables , and will be configuring them in the vars.tf files from now.

--- main.tf
+++ main.tf
@@ -21,17 +21,5 @@ output "s3id" {
   value = aws_s3_bucket.name.id
 }
 
-variable "buck-name" {
-  default = "buck-var-k21"
-}
-
-variable "user-name" {
-  description = "Enter your User Name"
-  default = "kingkongthegreatlegend"
-}
-
-variable "ins-name" {
-  default = "ins-k211"
-}
 
 
