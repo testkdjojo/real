37409546d5a3294d9a153e0d9b4eeef6e44a0b18
testkdjojo <davidokiji131@gmail.com>
2025-11-16 23:11:21 +0000

    Update vars.tf
    update to the variable config's

--- vars.tf
+++ vars.tf
@@ -1,1 +1,13 @@
+variable "buck-name" {
+  default = "buck-var-k21"
+}
+
+variable "user-name" {
+  description = "Enter your User Name"
+  default = "kingkongthegreatlegend"
+}
+
+variable "ins-name" {
+  default = "ins-k211"
+}
 
